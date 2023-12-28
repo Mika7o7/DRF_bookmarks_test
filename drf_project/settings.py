1262df0cd2e1842699fe0be0674a9ce6552946dc
Mika7o7 <mikaohanyan881@gmail.com>
2023-12-28 15:20:49 +0400

    update README.md and solve problem with static

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -146,12 +146,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# if DEBUG:
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 
